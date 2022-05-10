43267c51668fdf1a9076f460ad0456fa1ccd4d14
ErrNatsuki1666 <96864066+BkYuris@users.noreply.github.com>
2022-05-10 11:43:30 +0800

    Update and rename README.md to Script.lua

--- README.md
+++
@@ -1,3 +0,0 @@
-Script-CollectionUnlock all Hoverboard (PSX).lua
-Prison Life Admin (Septex).lua
-Prison Life Admin (Err Natsuki).lua
---
+++ Script.lua
@@ -0,0 +1,19 @@
+_G.EventList = {
+
+    [1] = 'Unlock all Hoverboard (PSX).lua',
+
+    [2] = 'Prison Life Admin (Septex).lua',
+
+    [3] = 'Prison Life Admin (Err Natsuki).lua'
+
+}
+
+-- # i dont know why i made this script lol :}
+
+-- # haha wtf is that going on here
+
+-- # what the hell is happening in here in
+
+-- # this is just event ik lol
+
+-- # wait are you pooping
